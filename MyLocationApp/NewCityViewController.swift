045d3253c1a7da82066611675cdc29bbb916da7b
MRSgkce <mursidegokce985@gmail.com>
2024-10-26 23:55:56 +0300

    şehirler gösteriliyor, yeni şehir ekleniyor ve silinebiiyor. şehir detayları üzerine  tıklanınca görüntülenebiliyor.

---
+++ MyLocationApp/NewCityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewCityViewController.swift
+//  MyLocationApp
+//
+//  Created by Mürşide Gökçe on 26.10.2024.
+//
+
+import UIKit
+
+class NewCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
